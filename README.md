6b54c41215ff27ab579e7c8eca733ac558674d7d
QUINOESCOBAR <quinoescobar@gmail.com>
2015-10-16 00:13:12 +0100

    funcionan algunos test, readme modificado

--- README.md
+++ README.md
@@ -5,8 +5,8 @@ La práctica consiste en realizar un conversor de temperaturas de grados Celsius
 
 Se ha utilizado Mocha , Chai y Gulp; Mocha y Chai son utilizados para la creación de las pruebas realizadas , se pueden ubicar dentro de la carpeta test.
 
-Enlace página práctica [Cálculadora temperatura](https://quinoescobar.github.io/sytw_prt_01)
+Enlace página práctica [Cálculadora temperatura](https://quinoescobar.github.io/sytw_prt_02)
 
-Enlace test de la práctica [Cálculadora temperatura](https://quinoescobar.github.io/sytw_prt_01/tests/index.html)
+Enlace test de la práctica [Cálculadora temperatura](https://quinoescobar.github.io/sytw_prt_02/tests/index.html)
 
 Enlace página author [Quinoescobar](https://quinoescobar.github.io)
--- tests/tests.js
+++ tests/tests.js
@@ -3,28 +3,37 @@ var expect = chai.expect;
 describe("Testing con BDD", function() {
 
   describe("Temperatura", function() {
+    it("Deberia poder crearse un objeto Temperatura", function() {
+      var paraCalcular = new Temperatura();
+      // var should = require('chai.js').should();
+      expect(paraCalcular).to.be.an('object');
+    });
     it("Deberia tener un campo valor", function() {
       var paraCalcular = new Temperatura();
-      paraCalcular.setValor(43);
+      paraCalcular.setValor(123456789);
       paraCalcular.setTipo("Faren");
-      expect(paraCalcular.getValor).to.equal("123456789");
+      expect(paraCalcular.getValor()).to.equal(123456789);
     });
 
-    it("should set cow's name if provided", function() {
-      var cow = new Cow("Kate");
-      expect(cow.name).to.equal("Kate");
+    it("Deberia tener un campo tipo", function() {
+      var paraCalcular = new Temperatura();
+      paraCalcular.setValor(123456789);
+      paraCalcular.setTipo("Farenheit");
+      expect(paraCalcular.getTipo()).to.equal("Farenheit");
     });
   });
 
-  describe("#calcular", function() {
-    it("should throw if no target is passed in", function() {
+  describe("#Calculando:Converting", function() {
+    it("Deberia sacar error al introducir 43X", function() {
       expect(function() {
         (new Temperatura()).calcular("43X");
       }).to.throw(Error);
     });
 
     it("should greet passed target", function() {
-      var calculado = (new Temperatura("43F")).calcular();
+      var calculado = new Temperatura();
+      calculado.setValor();
+      calculado.setTipo();
       expect(calculado).to.equal("6.1 Celsius");
     });
   });
