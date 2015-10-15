0deef8b5e077ba61156f9ced89d6e61a45ee585e
QUINOESCOBAR <quinoescobar@gmail.com>
2015-10-15 18:51:40 +0100

    creado los gets and sets de Medida

--- temperatura.js
+++ temperatura.js
@@ -4,9 +4,20 @@ function Medida(val,tip)
 {
   this.valor=val;
   this.tipo=tip;
-
 }
 
+Medida.prototype.getTipo = function () {
+  return this.tipo;
+};
+Medida.prototype.getValor = function () {
+  return this.valor;
+};
+Medida.prototype.setTipo = function (arg) {
+   this.tipo=arg;
+};
+Medida.prototype.setValor = function (arg) {
+   this.valor=arg;
+};
 
 function Temperatura()
 {
